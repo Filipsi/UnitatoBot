2bd198c054b13ecd6d4df7cb1a1cfefb75fccf60
Filipsi <Filip.sikula@seznam.cz>
2016-10-19 11:04:43 +0200

    RebootExecutor now shows who issued the reboot command after reboot is complete

--- UnitatoBot/UnitatoBot/Execution/Executors/RebootExecutor.cs
+++ UnitatoBot/UnitatoBot/Execution/Executors/RebootExecutor.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using UnitatoBot.Bridge;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace UnitatoBot.Execution.Executors {
 
@@ -21,11 +22,17 @@ namespace UnitatoBot.Execution.Executors {
                     data = reader.ReadToEnd();
                 }
 
-                ServiceMessage msg = JsonConvert.DeserializeObject<ServiceMessage>(data);
+                JObject jobject = JObject.Parse(data);
+
+                ServiceMessage msg = jobject.GetValue("Message").ToObject<ServiceMessage>();
                 msg = manager.FindServiceType(msg.ServiceType)[0].FindMessage(msg.Origin, msg.Id);
 
                 if(msg != null)
-                    msg.Edit("Reboot complete!");
+                    msg.Edit(new ResponseBuilder()
+                        .Text("Reboot requested by user")
+                        .Block(jobject.GetValue("Issuer").ToObject<string>())
+                        .Text("was completed!")
+                        .Build());
 
                 fReboot.Delete();
             }
@@ -44,10 +51,10 @@ namespace UnitatoBot.Execution.Executors {
         public ExecutionResult Execute(CommandContext context) {
             ServiceMessage msg = context.ResponseBuilder
                 .Username()
-                .Text("requested reboot. Reboot in progress...")
+                .Text("requested reboot. Restart in progress...")
                 .Send();
 
-            SaveRebootFile(msg);
+            SaveRebootFile(msg, context.ServiceMessage.Sender);
 
             Process.Start(Process.GetCurrentProcess().MainModule.FileName.Replace(".vshost", string.Empty));
             Environment.Exit(0);
@@ -56,9 +63,13 @@ namespace UnitatoBot.Execution.Executors {
 
         // Util
 
-        private void SaveRebootFile(ServiceMessage message) {
+        private void SaveRebootFile(ServiceMessage message, string issuer) {
+            JObject jobject = new JObject();
+            jobject.Add(new JProperty("Issuer", issuer));
+            jobject.Add(new JProperty("Message", JToken.FromObject(message)));
+
             using(StreamWriter writer = new StreamWriter("reboot.json", false)) {
-                writer.Write(JsonConvert.SerializeObject(message));
+                writer.Write(jobject.ToString());
             }
         }
 
