728e1a5feb26157a3e2620ba9cf330f6412c8c22
Filipsi <Filip.sikula@seznam.cz>
2017-01-14 12:32:04 +0100

    Added flip coin command

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # UnitatoBot
 
-[![Heroku](http://heroku-badge.herokuapp.com/?app=unitatobot&style=flat)]
+![Heroku](http://heroku-badge.herokuapp.com/?app=unitatobot&style=flat)
 
-Private chat bot with abstraction layer, currently supporting Discord
+Private chat bot with abstraction layer, currently for [Discord](https://discordapp.com/)
 
 Main branch is available [here](https://github.com/Filipsi/UnitatoBot/tree/production).
+
 Development branch with bleeding edge changes: [js](https://github.com/Filipsi/UnitatoBot/tree/js)
+
 Deprecated C# version can be found [here](https://github.com/Filipsi/UnitatoBot/tree/csharp).
---
+++ src/mapping/ExecutionContext.js
@@ -0,0 +1,23 @@
+const _ = require('lodash')
+
+module.exports = function (name, mapping, result, message) {
+  // Internals
+  const parseMachResult = () => {
+    // Remove arguments that are't dataholder
+    let dataholders = _.remove(result, (entry) => _.isArray(entry))
+    // Parse data to key/value format
+    return _.fromPairs(dataholders)
+  }
+
+  // Public interface
+  this.args = parseMachResult()
+
+  this.message = message
+
+  this.log = () => {
+    console.log(
+      'Executed command "' + name + '"' +
+      ' with mapping "' + mapping.map + '"' +
+      ' and arguments ' + JSON.stringify(this.args))
+  }
+}
