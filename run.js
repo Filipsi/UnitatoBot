e56c5808da3919966b486503839ad80bbb8f4234
Filipsi <cz.filipsi@gmail.com>
2017-03-25 20:07:24 +0100

    Fixed paths to work on Heroku

--- run.js
+++ run.js
@@ -1,23 +1,3 @@
-'use strict';
-
-/* Modules */
-const path = require('path');
-const spawn = require('child_process').spawn;
-
-/* Definitions */
-const args = [
-	'--use_strict', // Run app in strict mode
-	'UnitatoBot.js'
-];
-
-const opt = {
-	cwd: path.join(__dirname, 'src'),
-	env: (function () {
-		process.env.NODE_PATH = '.'; // Enables require() calls relative to the cwd :)
-		return process.env;
-	}()),
-	stdio: [process.stdin, process.stdout, process.stderr]
-};
-
-/* Run */
-spawn(process.execPath, args, opt);
+process.env.NODE_PATH = __dirname + '/src';
+require('module').Module._initPaths();
+require('UnitatoBot');
